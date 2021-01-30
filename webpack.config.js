cda25df1776e12edf7e07ec2e2f266cc5d5d763f
Ainika <aina.kostina@gmail.com>
2021-01-31 01:40:04 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+})
+
+module.exports = {
+    entry: "./src",
+    mode: "production",
+    performance: { hints: false },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
+    },
+    plugins: [htmlPlugin],
+};
